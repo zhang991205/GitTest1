b1aee8f2feded09173aff85c6de3918d8e4b3a5a
张晨阳 <68488773+zhang991205@users.noreply.github.com>
2022-02-09 16:55:51 +0800

    pull 2

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("ssh test");
         System.out.println("pull test");
+        System.out.println("pull test2");
     }
 }
