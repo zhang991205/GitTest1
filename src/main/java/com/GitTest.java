3daed013afcdd1fc1323435755032e323e0cd1c2
zcy <1905009509@qq.com>
2022-02-09 16:29:25 +0800

    ssh commit v1

--- src/main/java/com/GitTest.java
+++ src/main/java/com/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("ssh test");
     }
 }
